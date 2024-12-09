c4fde8c4215cf973b38ad23adaf4a31e221fa594
aditriber <adi4722@gmail.com>
2024-12-09 21:03:18 +0200

    Update codons.py

--- codons.py
+++ codons.py
@@ -1,13 +1,15 @@
 def create_codon_dict(file_path):
-    file = open(file_path)
-    rows = file.readlines()
-    file.colse()
+    with open(file_path) as file:
+        rows = file.readlines()
+    
     d = {}
     for row in rows:
-      cells = row.strip().split('\t')
-      codon = cells[0]
-      amino_acid = cells[2]
-      d[codon] = amino_acid
+        cells = row.strip().split('\t')
+        codon = cells[0]
+        amino_acid = cells[2]
+        d[codon] = amino_acid
+        
     return d
+
 
 
